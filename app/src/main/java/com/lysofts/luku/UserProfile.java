01b13b2b95577a188b51720cafeddc282a4984b9
ochom <ochomrichard752@gmail.com>
2020-05-12 14:19:00 +0300

    2

--- app/src/main/java/com/lysofts/luku/UserProfile.java
+++ app/src/main/java/com/lysofts/luku/UserProfile.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_profile);
     }
 
-    public void SelectGender(View view) {
-        startActivity(new Intent(MainActivity.this, UserProfile.class));
+    public void closeProfile(View view) {
+        startActivity(new Intent(UserProfile.this,MainActivity.class));
         finish();
     }
 }
